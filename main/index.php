3532a40789687355156c60749c6634be2d954b46
zzz2010 <zzz2010@07a4ff46-b0ea-8366-24e1-45e5083e2767>
2013-02-21 09:02:16 +0000

    initial commit

---
+++ main/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html>
+    <head>
+            
+        <title>Cytoscape Web</title>
+        
+        <link rel="shortcut icon" href="../img/layout/favicon.png" />
+        <meta name="keywords" content="Cytoscape, Cytoscape Web, visualization, interaction network, graph, bioinformatics, computational biology, pathways">
+        
+        
+        <link rel="stylesheet" type="text/css" href="../css/layout.css" />
+<link rel="stylesheet" type="text/css" href="../css/content.css" />
+<link rel="stylesheet" type="text/css" href="../js/jquery/jquery-ui/css/custom-theme/jquery-ui-1.7.2.custom.css" />
+<link rel="stylesheet" type="text/css" href="../css/content/demo.css" />
+        
+        <!--[if IE]>
+        <link rel="stylesheet" type="text/css" href="/css/content/demo.ie.css" />
+        <![endif]-->
+        
+        <script type="text/javascript" src="../js/jquery/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="../js/jquery/plugins/jquery.qtip-1.0.0-rc3.min.js"></script>
+<script type="text/javascript" src="../js/layout/layout.js"></script>
+<script type="text/javascript" src="../js/string/levenshtein.js"></script>
+<script type="text/javascript" src="../js/jquery/jquery-ui/js/jquery-ui-1.8.12.custom.min.js"></script>
+<script type="text/javascript" src="../js/flash/flash_detect_min.js"></script>
+<script type="text/javascript" src="../js/jquery/plugins/jquery.layout.min.js"></script>
+<script type="text/javascript" src="../js/jquery/plugins/jquery.menu.js"></script>
+<script type="text/javascript" src="../js/jquery/plugins/jquery.tablesorter.min.js"></script>
+<script type="text/javascript" src="../js/jquery/plugins/jquery.validate.js"></script>
+<script type="text/javascript" src="../js/jquery/plugins/jquery.thread.js"></script>
+<script type="text/javascript" src="../js/jquery/plugins/jquery.farbtastic.js"></script>
+<script type="text/javascript" src="../js/jquery/plugins/jquery.cytoscapeweb.js"></script>
+<script type="text/javascript" src="../js/cytoscape_web/json2.min.js"></script>
+<script type="text/javascript" src="../js/cytoscape_web/AC_OETags.min.js"></script>
+<script type="text/javascript" src="../js/cytoscape_web/cytoscapeweb.min.js"></script>
+<script type="text/javascript" src="../js/cytoscape_web/cytoscapeweb-styles-demo.js"></script>
+<script type="text/javascript" src="../js/cytoscape_web/cytoscapeweb-file.js"></script>
+<script type="text/javascript" src="../js/content/demo.js.php?url=<? echo $_GET["url"] ?>"></script>
+<script type="text/javascript" src="../js/util/ga.js"></script>
+        
+        <!--[if IE]>
+                <![endif]-->
+
+        
+        	</head>
+	
+    <body>
+	
+            
+            
+		  
+        <div id="page" class="slice">
+            
+                        
+            <!-- begin page content -->
+            <div id="content" class="half_and_half">
+            
+            <div class="left">
+    <h1>Please enable Javascript</h1>
+    <p>Javascript is not enabled in your browser, and it is necessary to have Javascript enabled to view this demo.</p>
+    <p>Please enable Javascript, and then reload this page.</p>
+</div>
+<div class="right">
+    <h1>What if my browser does not support Javascript?</h1>
+    <p>Please consider <a href="http://www.mozilla.com">upgrading your browser</a>.</p>
+</div>
+	
+	        </div>
+	        <!-- end page content -->
+        </div> 
+	
+
+    </body>
+
+</html>
